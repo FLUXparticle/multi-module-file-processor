9e409e9aa06c4fa77d6a66c2afa073201021b292
Sven Reinck <sreinck@fluxparticle.de>
2024-02-05 14:52:41 +0100

    Exec über Haupt-Projekt ausführen

--- module2/src/main/java/com/example/module2/Main.java
+++ module2/src/main/java/com/example/module2/Main.java
@@ -2,8 +2,16 @@ package com.example.module2;
 
 import com.example.module1.Greeter;
 
+import java.net.*;
+
 public class Main {
     public static void main(String[] args) {
+        URLClassLoader classLoader = (URLClassLoader) Main.class.getClassLoader();
+
+        for (URL url : classLoader.getURLs()) {
+            System.out.println("url = " + url);
+        }
+
         Greeter.print();
     }
 }
